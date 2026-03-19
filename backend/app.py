ea69205fe3d701944a718afc6b6462776f9293bd
Faridha-Banu <mullafaridhabanu@gmail.com>
2026-03-19 12:02:13 +0530

    Fixed deployment config

--- backend/app.py
+++ backend/app.py
@@ -1,5 +1,6 @@
 from flask import Flask, request, render_template
 import joblib
+import os
 
 app = Flask(__name__, template_folder='../templates')
 
@@ -48,4 +49,5 @@ def predict():
     )
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
